5744b3d70fa73e3f610996893727094d59de8a50
joe c <joec898@gmail.com>
2022-10-13 15:56:10 -0500

    upload new app

---
+++ src/router/index.js
@@ -0,0 +1,35 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import Products from '../views/ProductsView.vue'
+import PastOrders from '../views/PastOrders.vue'
+import About from '../views/AboutView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: About
+  },
+  {
+    path: '/products',
+    name: 'Products',
+    component: Products
+  },
+  {
+    path: '/past-orders',
+    name: 'PastOrders',
+    component: PastOrders
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
